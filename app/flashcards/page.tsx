d9e4d7f5a7be5fe78760a3907039215fd5a31081
phuctrank81 <131422071+phuctrank81@users.noreply.github.com>
2025-12-21 13:58:04 +0700

    new update

--- app/flashcards/page.tsx
+++ app/flashcards/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import Header from "../components/Header"
-import Footer from "../components/Footer"
+import Header from "@/components/Header"
+import Footer from "@/components/Footer"
 
 export default function FlashcardsPage() {
   return (
