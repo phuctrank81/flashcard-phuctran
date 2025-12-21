38b0ad3ba7fb1846806f0a50f147e6977ad00b9d
phuctrank81 <131422071+phuctrank81@users.noreply.github.com>
2025-12-21 13:56:39 +0700

    new update

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@
 import { BookOpen, GraduationCap, Zap, Trophy, Brain, Target } from "lucide-react"
 import Link from "next/link"
 import BannerSlider from "./components/BannerSlider"
-import Header from "@/app/components/Header"
-import Footer from "@/app/components/Footer"
+import Header from "@/components/Header"
+import Footer from "@/components/Footer"
 
 export default function HomePage() {
   return (
