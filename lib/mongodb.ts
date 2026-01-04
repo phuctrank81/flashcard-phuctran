a734e28d6384fb7f397a7fa33802b397d27b9a89
phuctrank81 <131422071+phuctrank81@users.noreply.github.com>
2026-01-04 15:56:35 +0700

    fix một lần nữa

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,32 +1,16 @@
-import mongoose, { Mongoose } from "mongoose";
+import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI as string;
 
-if (!MONGODB_URI) {
-  throw new Error("Missing MONGODB_URI");
-}
+// if (!MONGODB_URI) {
+//   throw new Error("Missing MONGODB_URI");
+// }
 
-declare global {
-  // eslint-disable-next-line no-var
-  var mongoose: {
-    conn: Mongoose | null;
-    promise: Promise<Mongoose> | null;
-  };
-}
-
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
-export default async function connectDB(): Promise<Mongoose> {
-  if (cached.conn) return cached.conn;
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI);
+export default async function connectDb() {
+  try{
+    await mongoose.connect(MONGODB_URI);
+    console.log("Connected to MongoDB");
+  }catch(error){
+    console.error("MongoDB connection error:", error);
   }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
 }
