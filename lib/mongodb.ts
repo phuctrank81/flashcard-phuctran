c6eb1e7dbf15a4bf77bbfbe65899ffa944340d70
phuctrank81 <131422071+phuctrank81@users.noreply.github.com>
2026-01-04 15:20:05 +0700

    upgrade

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,4 +1,4 @@
-import mongoose, { Mongoose } from "mongoose";
+import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI as string;
 
@@ -6,30 +6,11 @@ if (!MONGODB_URI) {
   throw new Error("Missing MONGODB_URI");
 }
 
-/**
- * Khai báo type cho global cache
- */
-declare global {
-  // eslint-disable-next-line no-var
-  var mongoose: {
-    conn: Mongoose | null;
-    promise: Promise<Mongoose> | null;
-  };
-}
-
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
-export default async function connectDB(): Promise<Mongoose> {
-  if (cached.conn) return cached.conn;
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI);
+export default async function connectDb() {
+  try{
+    await mongoose.connect(MONGODB_URI);
+    console.log("Connected to MongoDB");
+  }catch(error){
+    console.error("MongoDB connection error:", error);
   }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
 }
