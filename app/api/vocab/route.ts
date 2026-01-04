62dba82e385f7b9ab70d94577518f50bbf1a74e1
phuctrank81 <131422071+phuctrank81@users.noreply.github.com>
2026-01-04 16:12:49 +0700

    đã sửa rồi

--- app/api/vocab/route.ts
+++ app/api/vocab/route.ts
@@ -2,14 +2,15 @@ import { NextResponse } from "next/server";
 import connectDB from "@/lib/mongodb";
 import Words from "@/models/words";
 
+
 export async function GET() {
   try {
     await connectDB();
 
-    const words = await Words.find({ timeout: 3000 }).lean();
-
-    // Thêm độ trễ 30 giây
-    await new Promise(resolve => setTimeout(resolve, 30000));
+    const words = await Words
+      .find({})
+      .maxTimeMS(30000)
+      .lean();
 
     return NextResponse.json(words);
   } catch (error) {
