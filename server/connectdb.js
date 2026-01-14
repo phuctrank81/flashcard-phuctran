969d01940d7a199e77f40632e1007075a6670a7a
phuctrank81 <131422071+phuctrank81@users.noreply.github.com>
2026-01-14 08:20:08 +0700

    s

--- server/connectdb.js
+++ server/connectdb.js
@@ -10,7 +10,7 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGODB_URI,{
       dbName: 'words',
     });
-    console.log('Connected to MongoDB successfully');
+    console.log('Connected to MongoDB successfully ok');
   } catch (err) {
     console.error('Failed to connect to MongoDB:', err);
   }
