726c4a027e4354d8aad2591ae5ef2e3cc419468c
phuctrank81 <131422071+phuctrank81@users.noreply.github.com>
2026-01-15 09:04:32 +0700

    new update

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 8000;  // ← Thay đổi 1: Thêm process.env.PORT
+const port = process.env.PORT || 10000;  // ← Thay đổi 1: Thêm process.env.PORT
 const connectDB = require('./connectdb');
 const morgan = require('morgan');
 const cors = require('cors');
