63a742faf44fbc0b17de822d9a5d3b54012618a5
phuctrank81 <131422071+phuctrank81@users.noreply.github.com>
2026-02-08 13:02:13 +0700

    ok

--- app/flashcards/page.tsx
+++ app/flashcards/page.tsx
@@ -59,7 +59,7 @@ export default async function FlashcardsPage() {
                   </h2>
 
                   <p className="text-slate-700 mb-1">
-                    <b>Nghĩa:</b> {item.definition}
+                    <b>Definition:</b> {item.definition}
                   </p>
 
                   {item.example && (
