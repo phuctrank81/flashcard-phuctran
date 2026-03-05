7bba2e3013057a02d0d3465ee21114a404964ac8
Phuc <tranthienphuck81@gmail.com>
2026-03-05 13:45:10 +0700

    feat: create FlashcardsPage component with Header and Footer

---
+++ app/document/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+import { headers } from "next/headers";
+
+export default async function FlashcardsPage() {
+    return(
+        <div>
+            <Header />
+            <Footer />
+        </div>
+    )
+  
+}
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import BannerSlider from "../components/banner"
 import Header from "../components/header"
 import Footer from "../components/footer"
 
+
 export default function HomePage() {
   return (
     <div className="min-h-screen bg-slate-50 flex flex-col">
