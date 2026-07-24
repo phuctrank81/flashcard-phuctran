280a00a7690411c0eb51337827f40ddc900adb5c
Phuc <minecraft811500@gmail.com>
2026-07-24 20:19:11 +0700

    chore: clean up unused code and improve readability in DocumentPage component

--- app/document/page.tsx
+++ app/document/page.tsx
@@ -51,7 +51,7 @@ export default function DocumentPage() {
   const currentDocs = activeSeries ? seriesGroups[activeSeries] || [] : [];
 
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="min-h-screen flex flex-col">
       <Header />
 
       {previewDoc && (
